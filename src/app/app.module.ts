ebf97fc6f608a22dd9ce967a15c8d23dab34d859
Muhammad Talha <talhaguy@gmail.com>
2021-02-28 01:24:51 -0800

    reorganize imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,50 +2,52 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { InputTextModule } from 'primeng/inputtext';
 import { ToastModule } from 'primeng/toast';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { WeatherApiKeyInterceptor } from './interceptors/weather-api-key.interceptor';
+import { DatePipe } from '@angular/common';
+import { IsUndefinedPipe } from './pipes/is-undefined.pipe';
+import { WeatherIconUrlPipe } from './pipes/weather-icon-url.pipe';
+import { UnitConversionPipe } from './pipes/unit-conversion.pipe';
+
 import { AppComponent } from './app.component';
-import { LocationPermissionDialogComponent } from './components/location-permission-dialog/location-permission-dialog.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { LocationPermissionDialogComponent } from './components/location-permission-dialog/location-permission-dialog.component';
 import { LocationPermissionStatusComponent } from './components/location-permission-status/location-permission-status.component';
 import { LocationDetailsComponent } from './components/location-details/location-details.component';
 import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
 import { WeatherForecastComponent } from './components/weather-forecast/weather-forecast.component';
 import { LocationEntryFormComponent } from './components/location-entry-form/location-entry-form.component';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'src/environments/environment';
-import { ConfirmationService, MessageService } from 'primeng/api';
+import { WeatherForecastDayComponent } from './components/weather-forecast-day/weather-forecast-day.component';
+import { WeatherForecastHourChartComponent } from './components/weather-forecast-hour-chart/weather-forecast-hour-chart.component';
+
 import { reducers } from './store/app.reducer';
 import { effects } from './store/app.effects';
-import { WeatherApiKeyInterceptor } from './interceptors/weather-api-key.interceptor';
-import { IsUndefinedPipe } from './pipes/is-undefined.pipe';
-import { WeatherForecastDayComponent } from './components/weather-forecast-day/weather-forecast-day.component';
-import { WeatherIconUrlPipe } from './pipes/weather-icon-url.pipe';
-import { UnitConversionPipe } from './pipes/unit-conversion.pipe';
-import { FormsModule } from '@angular/forms';
-import { WeatherForecastHourChartComponent } from './components/weather-forecast-hour-chart/weather-forecast-hour-chart.component';
-import { DatePipe } from '@angular/common';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
+    IsUndefinedPipe,
+    WeatherIconUrlPipe,
+    UnitConversionPipe,
     AppComponent,
-    LocationPermissionDialogComponent,
     MainPageComponent,
+    LocationPermissionDialogComponent,
     LocationPermissionStatusComponent,
     LocationDetailsComponent,
     CurrentWeatherComponent,
     WeatherForecastComponent,
     LocationEntryFormComponent,
-    IsUndefinedPipe,
     WeatherForecastDayComponent,
-    WeatherIconUrlPipe,
-    UnitConversionPipe,
     WeatherForecastHourChartComponent,
   ],
   imports: [
